dce800405af16328d9c086a14529360b490762a6
jakyyy69 <prijavlanjenasranje@gmail.com>
2018-10-08 12:55:32 +0200

    Dodajanje

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prijavaBtn = new System.Windows.Forms.Button();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prijavaBtn
+            // 
+            this.prijavaBtn.Location = new System.Drawing.Point(80, 166);
+            this.prijavaBtn.Name = "prijavaBtn";
+            this.prijavaBtn.Size = new System.Drawing.Size(75, 23);
+            this.prijavaBtn.TabIndex = 0;
+            this.prijavaBtn.Text = "button1";
+            this.prijavaBtn.UseVisualStyleBackColor = true;
+            this.prijavaBtn.Click += new System.EventHandler(this.prijavaBtn_Click);
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(125, 50);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 1;
+            // 
+            // passTextBox
+            // 
+            this.passTextBox.Location = new System.Drawing.Point(125, 93);
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.prijavaBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button prijavaBtn;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static string user = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void prijavaBtn_Click(object sender, EventArgs e)
+        {
+            runLogin();
+        }
+
+        private void runLogin()
+        {
+            string komanda = "SELECT username FROM uporabniki WHERE ((username= '" + usernameTextBox.Text + "') AND (pass ='" + passTextBox.Text + "'));";
+            /*   string selekt = "SELECT username, pass FROM uporabniki";
+               string mySQLConnectionString = "datasource=mysql6001.site4now.net;username=a41906_projekt;password=salabajzer123;database=uporabniki";
+
+               MySqlConnection con = new MySqlConnection(mySQLConnectionString);
+               MySqlCommand com = new MySqlCommand(selekt, con);
+
+               con.Open();
+               MySqlDataReader reader = com.ExecuteReader();*/
+
+            using (MySqlConnection conn = new MySqlConnection("datasource = mysql6001.site4now.net; username = a41906_projekt; password = salabajzer123; database = db_a41906_projekt; sslmode=none"))
+            {
+                conn.Open();
+                using (MySqlCommand com = new MySqlCommand(komanda, conn))
+                {
+                    MySqlDataReader reader = com.ExecuteReader();
+                    reader.Read();
+                    try
+                    {
+                        //user = reader["username"].ToString();
+                        MessageBox.Show("prijava uspesna");
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        MessageBox.Show("Napačno uporabniško ime ali geslo.", "Neuspešna prijava!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    com.Dispose();
+                }
+                conn.Close();
+            }
+
+        }
+    }
+}
